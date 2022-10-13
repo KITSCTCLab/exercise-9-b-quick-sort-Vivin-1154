a10455c7a798ddb1036d727e65bde5c59fb7de6f
VIVIN AATHAV URK21CS1154 <110010382+Vivin-1154@users.noreply.github.com>
2022-10-13 21:01:22 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,7 +7,7 @@ input_data = input()
 data = []
 for item in input_data.split(', '):
   if item.isnumeric():
-    data.append(int(item))
+    data.append(int(it)
   elif item.lstrip("-").isnumeric():
     data.append(int(item))
 print(quick_sort(data, 0, len(data)-1))
